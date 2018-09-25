f197d0eb53b5e20554397ffc3d08f67ab88ceca6
Dave Turner <D_Turner_Dev@Outlook.com>
2018-09-25 11:33:51 +0100

    Added Contact page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+
+// function initMap() {
+//     let loc = {lat: 53.394 ,lng: -1.529};  // Change this.
+//     let map = new google.maps.Map(
+//         document.getElementById('map-container-7'), {zoom: 4, center: loc});
+//     let marker = new google.maps.Marker({position: loc, map: map});
+//     console.info("hit");
+//
+//  }
+
 leave_sites = [
     "https://www.amazon.co.uk",
     "https://www.bbc.co.uk/news"
